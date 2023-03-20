be70a0307969bd044f7f7fb3646b0b9145cdd8e7
zk <zk@study.com>
2023-03-20 19:54:53 +0800

    2-commit

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("1111222");
         System.out.println(222222);
+        System.out.println(222222);
     }
 }

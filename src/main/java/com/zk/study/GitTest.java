3eeaf8d357b4309e38557f4aa8f6feb6ecd93d4a
zk <zk@study.com>
2023-03-20 19:46:57 +0800

    1-commit

---
+++ src/main/java/com/zk/study/GitTest.java
@@ -0,0 +1,12 @@
+package com.zk.study;
+
+/**
+ * @author ZhouKai
+ * @create 2023-03-20 19:41
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("1111222");
+        System.out.println(222222);
+    }
+}

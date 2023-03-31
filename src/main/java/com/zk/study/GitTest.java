4f22a125cfe445495c71326d0101e25101ea55fa
zk <zk@study.com>
2023-03-31 22:07:46 +0800

    33

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -7,11 +7,5 @@ package com.zk.study;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("main");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("44");
-        System.out.println("55");
     }
 }

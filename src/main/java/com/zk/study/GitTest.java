6e0e65655da3bbad24215358f34ab01f2c50b51d
zk <zk@study.com>
2023-04-01 07:49:13 +0800

    2

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("main");
         System.out.println("1");
+        System.out.println("2");
         
     }
 }
--- src/main/java/com/zk/study/Test2.java
+++ src/main/java/com/zk/study/Test2.java
@@ -8,5 +8,6 @@ public class Test2 {
   public static void main(String[] args) {
     System.out.println("main-Test");
     System.out.println("1-Test");
+    System.out.println("2-Test");
   }
 }

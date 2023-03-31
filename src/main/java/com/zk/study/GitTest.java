1a62b90f54cd361d697e7b8d09cd0f13ab8c764a
zk <zk@study.com>
2023-03-31 20:52:24 +0800

    add-test

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println(44444);
         System.out.println("hot-fix");
         System.out.println("hot-fix2222");
+        System.out.println("tesst");
     }
 }

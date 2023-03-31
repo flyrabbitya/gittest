3fade59772ba2789afd5bd8a4e98f4311300c761
zk <zk@study.com>
2023-03-31 22:37:06 +0800

    bbb

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -7,18 +7,5 @@ package com.zk.study;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("main");
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("44");
-        System.out.println("55");
-        System.out.println("66");
-        System.out.println("77");
-        System.out.println("main2");
-        System.out.println("main3");
-        System.out.println("copy1");
-        System.out.println("copy2");
-        System.out.println("ccccccc");
-        System.out.println("bbb");
     }
 }

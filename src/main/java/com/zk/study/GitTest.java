65c4553f8abe0578545d044a5fc8f51b234d05b4
zk <zk@study.com>
2023-04-01 08:00:28 +0800

    12333

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -6,7 +6,7 @@ package com.zk.study;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("main");
+        System.out.println("main123333");
         System.out.println("1");
         System.out.println("2");
     }

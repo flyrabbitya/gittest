ad9101c8b9cbe3c0becdf90dba19f380467cee04
zk <zk@study.com>
2023-04-01 09:08:58 +0800

    2

--- src/main/java/com/zk/study/GitTest.java
+++ src/main/java/com/zk/study/GitTest.java
@@ -6,7 +6,7 @@ package com.zk.study;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git1");
+        System.out.println("git01");
         System.out.println("git2");
     }
 }
--- src/main/java/com/zk/study/Test2.java
+++ src/main/java/com/zk/study/Test2.java
@@ -6,7 +6,7 @@ package com.zk.study;
  */
 public class Test2 {
   public static void main(String[] args) {
-   System.out.println("test1");
+   System.out.println("test01");
     System.out.println("test2");
     }
 }

a0a3bdde01cd6a18842fdf5277b698c6c5a3343f
flyrabbitya <104545376+flyrabbitya@users.noreply.github.com>
2023-04-01 08:16:00 +0800

    1

--- src/main/java/com/zk/study/Test2.java
+++ src/main/java/com/zk/study/Test2.java
@@ -6,8 +6,6 @@ package com.zk.study;
  */
 public class Test2 {
   public static void main(String[] args) {
-     System.out.println("main-Test-1");
-    System.out.println("main-Test-2");
     System.out.println("main-Test-3");
   }
 }

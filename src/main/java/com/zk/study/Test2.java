74a2eb66f2a3998b35eb504a1fc8deb00d60107f
zk <zk@study.com>
2023-04-01 13:57:02 +0800

    b

--- src/main/java/com/zk/study/Test2.java
+++ src/main/java/com/zk/study/Test2.java
@@ -7,5 +7,8 @@ package com.zk.study;
 public class Test2 {
   public static void main(String[] args) {
     System.out.println("host1");
+    System.out.println("host1");
+    System.out.println("host1");
+    System.out.println("host1");
   }
 }

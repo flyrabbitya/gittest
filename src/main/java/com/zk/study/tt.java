b94672c249899b40f7d53dffbb411d664f0b90e9
zk <zk@study.com>
2023-04-02 09:46:06 +0800

    33

--- src/main/java/com/zk/study/tt.java
+++ src/main/java/com/zk/study/tt.java
@@ -6,9 +6,6 @@ package com.zk.study;
  */
 public class Test2 {
   public static void main(String[] args) {
-1111111111111111111111111111111111111111112
-      33333333333333333333333333333
-              454678
-                      7
+
   }
 }
